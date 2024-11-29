514aa6dd295e31e1debd20b71397cc30a3843f76
Natarajan <nataraj.itsolution@gmail.com>
2024-11-29 20:57:31 +0530

    Signal input output

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,9 +1,11 @@
 import { Routes } from '@angular/router';
 import { ListComponent } from './component/list/list.component';
 import { CreateComponent } from './component/create/create.component';
+import { CalcComponent } from './component/calc/calc.component';
 
 export const routes: Routes = [
     { path: 'invoice', component: ListComponent },
     { path: 'invoice/create', component: CreateComponent },
-    { path: 'invoice/edit/:invoiceno', component: CreateComponent }
+    { path: 'invoice/edit/:invoiceno', component: CreateComponent },
+    {path: 'calc', component: CalcComponent }
 ];
---
+++ src/app/component/calcbtn/calcbtn.component.ts
@@ -0,0 +1,36 @@
+import { Component, computed, effect, EventEmitter, input, Input, OnChanges, output, Output, SimpleChanges } from '@angular/core';
+import { MatButtonModule } from '@angular/material/button';
+
+@Component({
+  selector: 'app-calcbtn',
+  standalone: true,
+  imports: [MatButtonModule],
+  templateUrl: './calcbtn.component.html',
+  styleUrl: './calcbtn.component.css'
+})
+export class CalcbtnComponent {
+
+  // ngOnChanges(changes: SimpleChanges): void {
+  //   this.Squarevalue = this.height * this.width;
+  // }
+
+  // @Input({ required: true }) height: number = 0;
+  // @Input({ required: true }) width: number = 0;
+
+  //@Output() transferdata=new EventEmitter<number>()
+  constructor(){
+    effect(()=>this.transferdata.emit(this.Squarevalue()))
+  }
+  transferdata=output<number>({alias:'confirmdata'});
+
+  height=input.required<number>({alias:'ht'})
+  width=input(0)
+
+  //Squarevalue = this.height * this.width;
+
+  Squarevalue=computed(()=>this.height() * this.width())
+
+  SendData(){
+   this.transferdata.emit(this.Squarevalue())
+  }
+}
